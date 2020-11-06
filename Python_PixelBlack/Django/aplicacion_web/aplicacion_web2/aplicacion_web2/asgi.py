acdd7817abd708a454c383a6a1be4938b1c50fd0
danrodaba <69010048+danrodaba@users.noreply.github.com>
2020-11-06 23:26:30 +0100

    Aplicacion web Django

--- Python_PixelBlack/Django/aplicacion_web/aplicacion_web2/aplicacion_web2/asgi.py
+++ Python_PixelBlack/Django/aplicacion_web/aplicacion_web2/aplicacion_web2/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for anuncios2 project.
+ASGI config for aplicacion_web2 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'anuncios2.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aplicacion_web2.settings')
 
 application = get_asgi_application()

e135ad4639134a9caa08de6a722044abf1eca130
danrodaba <69010048+danrodaba@users.noreply.github.com>
2020-11-07 00:49:23 +0100

    Django

--- Python_PixelBlack/Django/aplicacion_web/anuncios/models.py
+++ Python_PixelBlack/Django/aplicacion_web/anuncios/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Anuncio(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published')
+    )
+    titulo = models.CharField(max_length=150)
+    texto = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    estado = models.Charfield(max_length=10,
+                            chices=STATUS_CHOICES,
+                            default='draft')
+    class Meta:     #creamos la clase metadatos
+        ordering = ('-created',)
+    def __str__(self):  #funcion que devuelve el título
+        return self.titulo

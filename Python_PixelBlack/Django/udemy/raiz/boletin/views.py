dc7cb695968c93efc1aface6f4bcd8d8ed49ef57
danrodaba <69010048+danrodaba@users.noreply.github.com>
2020-12-23 04:49:50 +0100

    djudemy

--- Python_PixelBlack/Django/udemy/raiz/boletin/views.py
+++ Python_PixelBlack/Django/udemy/raiz/boletin/views.py
@@ -5,7 +5,7 @@ from .forms import RegForm
 # Create your views here.
 def inicio(request):
     form = RegForm()
-    context ={
-        'el_form':form,
+    context = {
+        'el_form': form,
     }
-    return render(request,'inicio.html',context)
+    return render(request, 'inicio.html', context)
